2fbb44b823d84a0b05ec2fe98db880de47183e20
balac <balac@DESKTOP-6BQ07QP>
2024-10-28 19:58:58 +0530

    Day Two

---
+++ C2TC_BALACHANDAR/src/com/tns/ifet/daytwo/BreakDemo.java
@@ -0,0 +1,14 @@
+package com.tns.ifet.daytwo;
+
+public class BreakDemo {
+
+	public static void main(String[] args) {
+		for (int i = 5; i < 10; i++) 
+		{
+			System.out.println(i);
+			if (i == 5) {
+				break;
+			}
+		}
+	}
+}
---
+++ C2TC_BALACHANDAR/src/com/tns/ifet/daytwo/IfDemo.java
@@ -0,0 +1,12 @@
+package com.tns.ifet.daytwo;
+
+public class IfDemo {
+
+	public static void main(String[] args) {
+		int x = 7;
+		if (x != 5)
+		System.out.println("Value of x is not 5");
+	}
+
+}
+
